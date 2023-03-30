02a9d945fa813bb291159e08c2241d1ae54ccdf4
Jose Vergara <117672114+Josveco123@users.noreply.github.com>
2023-03-30 17:58:50 -0500

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,15 @@
+
+const miImagen = document.getElementById('imagenqr');
+
+const linkInicio = document.getElementById('aqr');
+
+linkInicio.addEventListener('click', function(event) {
+  // Evita que el enlace cambie de página
+  event.preventDefault();
+
+  if (miImagen.style.display === 'none') {
+    miImagen.style.display = 'block';
+  } else {
+    miImagen.style.display = 'none';
+  }
+});
